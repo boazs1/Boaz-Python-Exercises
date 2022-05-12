907f06a3a1b4987c38f9e56ca449b2d1fdd1d8a9
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 08:48:13 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -2,10 +2,6 @@ import csv
 import rook
 
 rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f', labels={"env":"dev"})
-if __name__ == "__main__":
-    rook.start(token='[7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f]',
-               labels={"env": "dev"}) # Optional,see Labels page below Projects
-    # Your program starts here :)
 file = open("\\temp\status_update.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
