35dab87f55901a77969dcfe73e6a3a7e3f6ba9b2
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 19:28:10 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -5,8 +5,7 @@ import urllib.request
 
 rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f',
     labels=None,
-    git_commit='bfdd9510bfcbb7c9b990d10a9fb6d3356abad565',
-    git_origin='https://github.com/boazs1/Boaz-Python-Exercises'
+ 
    )
 i = 1
 
