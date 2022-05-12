4f1711f2fbf6a95763c33969a56dafa37d1925b2
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 13:07:09 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -2,15 +2,10 @@ import csv
 import rook
 
 rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f',
-host='ROOKOUT_CONTROLLER_HOST',
-    port=None,
-    debug='ROOKOUT_DEBUG',
-    throw_errors='True',
-    log_to_stderr=None,
     labels=None,
     git_commit='bfdd9510bfcbb7c9b990d10a9fb6d3356abad565',
-    git_origin='https://github.com/boazs1/Boaz-Python-Exercises',
-    fork='ROOKOUT_ENABLE_FORK')
+    git_origin='https://github.com/boazs1/Boaz-Python-Exercises'
+)
 
 file = open("\\temp\status_update.csv")
 csvreader = csv.reader(file)
