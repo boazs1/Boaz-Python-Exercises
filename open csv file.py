92fef795449f9b8859d24cb6d6303615b8007786
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 14:33:34 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -1,11 +1,22 @@
 import csv
 import rook
 
+import urllib.request
+
 rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f',
     labels=None,
     git_commit='bfdd9510bfcbb7c9b990d10a9fb6d3356abad565',
     git_origin='https://github.com/boazs1/Boaz-Python-Exercises'
-)
-while i < 1000000000:
-  print(i)
-  i += 1
+   )
+i = 1
+
+
+fp = urllib.request.urlopen("http://www.ynet.co.il")
+mybytes = fp.read()
+
+mystr = mybytes.decode("utf8")
+fp.close()
+print(mystr)
+while i < 1000:
+    print(mystr)
+    i += 1
