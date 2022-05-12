7ed48e857df111bc2701436eb2eccc087c454089
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 09:48:35 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -1,17 +1,21 @@
 import csv
 import rook
-rook.start
-(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f', labels={"env":"dev"},
-    host=None,
+
+rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f', labels={"env":"dev"},
+host='ROOKOUT_CONTROLLER_HOST',
     port=None,
-    debug=None,
-    throw_errors=None,    
+    debug='ROOKOUT_DEBUG',
+    throw_errors=None,
     log_to_stderr=None,
-    labels=None,
-    git_commit=None,
-    git_origin=None,
-    fork=None,
-    **kwargs)
+    git_commit='ROOKOUT_COMMIT',
+    git_origin='ROOKOUT_REMOTE_ORIGIN',
+    fork='ROOKOUT_ENABLE_FORK')
+
+
+if __name__ == "__main__":
+    rook.start(token='[7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f]',
+               labels={"env": "dev"}) # Optional,see Labels page below Projects
+    # Your program starts here :)
 file = open("\\temp\status_update.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
