30316b414e0093c66697553cb51820abbac29b7f
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 13:30:55 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -6,17 +6,6 @@ rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a8
     git_commit='bfdd9510bfcbb7c9b990d10a9fb6d3356abad565',
     git_origin='https://github.com/boazs1/Boaz-Python-Exercises'
 )
-
-file = open("\\temp\status_update.csv")
-csvreader = csv.reader(file)
-header = next(csvreader)
-print(header)
-rows = []
-for row in csvreader:
-    rows.append(row)
-print(rows)
-file.close()
-i = 1
 while i < 1000000000:
   print(i)
   i += 1
