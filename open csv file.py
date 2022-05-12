8544d6d5c6d73f9d8d1870ace20b9cbc93db4718
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 09:52:30 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -11,11 +11,6 @@ host='ROOKOUT_CONTROLLER_HOST',
     git_origin='ROOKOUT_REMOTE_ORIGIN',
     fork='ROOKOUT_ENABLE_FORK')
 
-
-if __name__ == "__main__":
-    rook.start(token='[7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f]',
-               labels={"env": "dev"}) # Optional,see Labels page below Projects
-    # Your program starts here :)
 file = open("\\temp\status_update.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
