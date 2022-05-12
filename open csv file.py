48366dfb4931a2926e2bfc01b991e7e94e67cf41
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-12 09:33:58 +0300

    Update open csv file.py

--- open csv file.py
+++ open csv file.py
@@ -1,6 +1,17 @@
 import csv
 import rook
-rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f', labels={"env":"dev"})
+rook.start
+(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f', labels={"env":"dev"},
+    host=None,
+    port=None,
+    debug=None,
+    throw_errors=None,    
+    log_to_stderr=None,
+    labels=None,
+    git_commit=None,
+    git_origin=None,
+    fork=None,
+    **kwargs)
 file = open("\\temp\status_update.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
