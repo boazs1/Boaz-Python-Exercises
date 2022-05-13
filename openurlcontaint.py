dd8055de62f67004cdd2262c97b8f91fbbcb3ebb
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-13 07:36:26 +0300

    Add files via upload

---
+++ openurlcontaint.py
@@ -0,0 +1,14 @@
+import urllib.request
+
+fp = urllib.request.urlopen("http://www.ynet.co.il")
+mybytes = fp.read()
+
+mystr = mybytes.decode("utf8")
+fp.close()
+i = 1
+while i < 1000000:
+    print(mystr)
+    i = i + 1
+
+#result = mystr.find("boaz")
+#print(result);
