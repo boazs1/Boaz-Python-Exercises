a56faf681202655589515d31ea63833e48c2b07b
boazs1 <101975041+boazs1@users.noreply.github.com>
2022-05-14 12:19:38 +0300

    Update openurlcontaint.py

--- openurlcontaint.py
+++ openurlcontaint.py
@@ -3,7 +3,7 @@ import rook
 from flask import Flask, render_template
 app = Flask(__name__)
 rook.start(token='7aec038f4253ae4d0056ea75d634db2f5d45a3c885cfeb527b20e0b1bcd4a86f')
-app.run(host="0.0.0.0", port=5000, threaded=True)
+
 fp = urllib.request.urlopen("http://www.ynet.co.il")
 mybytes = fp.read()
 
